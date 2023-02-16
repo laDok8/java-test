175b4d8467cdfa032c275284ecd1448e5f764c38
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-02-16 10:45:22 +0100

    add fail

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,14 @@ public class Main {
         System.out.println("Hello world!");
         File dir = null;
         int random = (int) (Math.random() * 2);
-        if(random == 0) {
+        if (random == 0) {
             dir = new File("test");
         }
         createDir(dir);
         System.out.printf("random = %d", random);
         printSomething();
+
+        another_fail(null);
     }
 
     public static void printSomething() {
@@ -25,7 +27,12 @@ public class Main {
 
     public static void createDir(File dir) {
         if (dir != null || !dir.exists()) // BAD
-        //if (dir != null && !dir.exists()) // GOOD
+            //if (dir != null && !dir.exists()) // GOOD
             dir.mkdir();
+    }
+
+
+    public static boolean another_fail(File dir) {
+        return dir.exists(); // BAD
     }
 }
