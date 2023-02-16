974c3f422ba195f8734910a803f137c9046f4fa4
Ladislav Dokoupil <ldokoupi@redhat.com>
2023-02-16 09:55:41 +0100

    ferst

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+//import groovy.util.logging.Log4j;
+
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello world!");
+        File dir = null;
+        int random = (int) (Math.random() * 2);
+        if(random == 0) {
+            dir = new File("test");
+        }
+        createDir(dir);
+    }
+
+
+    public static void createDir(File dir) {
+        //if (dir != null || !dir.exists()) // BAD
+        if (dir != null && !dir.exists()) // GOOD
+            dir.mkdir();
+    }
+}
